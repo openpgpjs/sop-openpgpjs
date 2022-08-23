997c843a97683768817c8c10ee6a859c3579c073
Daniel Huigens <d.huigens@protonmail.com>
2022-08-23 17:34:48 +0200

    Implement inline-detach

---
+++ inlineDetach.js
@@ -0,0 +1,48 @@
+const openpgp = require('openpgp');
+const fs = require('fs');
+const process = require('process');
+const utils = require('./utils');
+
+const BAD_DATA = 41;
+
+const inlineDetach = async (signaturesOut, armor) => {
+  const data = utils.read_stdin();
+  let message;
+  try {
+    message = await openpgp.readMessage({ binaryMessage: data });
+  } catch (e) {
+    try {
+      message = await openpgp.readMessage({ armoredMessage: data.toString('utf8') });
+    } catch (e) {
+      try {
+        message = await openpgp.readCleartextMessage({ cleartextMessage: data.toString('utf8') });
+      } catch (e) {
+        console.error(e);
+        return process.exit(BAD_DATA);
+      }
+    }
+  }
+
+  const options = {
+    message,
+    verificationKeys: []
+  };
+
+  openpgp.verify(options).then(async (sig) => {
+    const signaturePackets = new openpgp.PacketList();
+    for (const s of sig.signatures) {
+      const signature = await s.signature;
+      signaturePackets.push(signature.packets[0]);
+    }
+    if (signaturesOut) {
+      const signatures = new openpgp.Signature(signaturePackets)[armor ? 'armor' : 'write']();
+      fs.writeFileSync(signaturesOut, signatures);
+    }
+    process.stdout.write(sig.data);
+  }).catch((e) => {
+    console.error(e);
+    return process.exit(BAD_DATA);
+  });
+};
+
+module.exports = inlineDetach;
