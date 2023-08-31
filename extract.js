3177f94c08f30060bb570e8fa63d0e3a760bee8f
larabr <7375870+larabr@users.noreply.github.com>
2023-08-31 15:44:19 +0200

    Fix `extract-cert`: read certificate from stdin

--- extract.js
+++ extract.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const process = require('process');
 const utils = require('./utils');
 
-const extract = async (armor, certfile) => {
-  const certs = await utils.load_keys(certfile);
-  const cert = certs[0];
-  const pubKey = cert.toPublic();
+const extract = async (armor) => {
+  const [privateKey] = await utils.load_keys('/dev/stdin');
+
+  const pubKey = privateKey.toPublic();
   if (!armor) {
     fs.writeSync(1, pubKey.toPacketlist().write());
     return;
