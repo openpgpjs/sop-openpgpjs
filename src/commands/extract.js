bb9f3830f5070075c79fc31a63493b7de9ebc5c7
Daniel Huigens <d.huigens@protonmail.com>
2024-11-13 12:51:56 +0100

    Handle errors in `sop extract-cert`

--- src/commands/extract.js
+++ src/commands/extract.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
 const process = require('process');
 const utils = require('../utils');
+const { BAD_DATA } = require('../errorCodes');
 
 const extract = async (armor) => {
   const [privateKey] = await utils.load_keys('/dev/stdin');
 
-  const pubKey = privateKey.toPublic();
-  if (!armor) {
-    fs.writeSync(1, pubKey.toPacketlist().write());
-    return;
+  try {
+    const pubKey = privateKey.toPublic();
+    if (!armor) {
+      fs.writeSync(1, pubKey.toPacketlist().write());
+      return;
+    }
+    process.stdout.write(pubKey.armor());
+  } catch (e) {
+    console.error(e.message);
+    return process.exit(BAD_DATA);
   }
-  process.stdout.write(pubKey.armor());
 };
 
 module.exports = extract;
