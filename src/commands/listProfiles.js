5d9f32b186ed74c8c0c007c1795ac0f2c45ff32a
Daniel Huigens <d.huigens@protonmail.com>
2024-11-13 13:27:25 +0100

    Remove DISABLE_PROFILES env variable
    It's no longer needed since we now always use OpenPGP.js v6 on the main
    branch. For testing OpenPGP.js v5, the openpgpjs-v5 branch can be used.

--- src/commands/listProfiles.js
+++ src/commands/listProfiles.js
@@ -3,9 +3,6 @@ const { UNSUPPORTED_PROFILE } = require('../errorCodes');
 const PROFILES = require('../profiles');
 
 const listProfiles = (subcommand) => {
-  if (process.env.DISABLE_PROFILES?.toLowerCase() === 'true') {
-    return;
-  }
   const supportedProfiles = Object.entries(PROFILES[subcommand] || {});
   if (!supportedProfiles.length) {
     console.error('no supported profiles');
